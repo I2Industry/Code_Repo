3d62afabadfd7cc009039fe8a5512983a6fe40fc
NSI1001 <SIVAKUMAR.N@nielseniq.com>
2022-12-27 16:16:04 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { ICellRendererParams } from 'ag-grid-community/dist/lib/main';
 
 @Component({
   selector: 'app-root',
@@ -9,59 +10,135 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   myForm!: FormGroup;
-
+  isForum = true;
   envs = ['US-UAT', 'EU-UAT'];
   response = '';
 
-   //hiding repo
-   isRepo:boolean = false
+  //hiding repo
+  isRepo: boolean = false
 
 
-   //onclick toggling both
-   onclickRepoCheck()
-   {
-     this.isRepo = !this.isRepo
-   }
+  //onclick toggling both
+  onclickRepoCheck() {
+    this.isRepo = !this.isRepo
+  }
 
   //hiding egg_build
-  isBuild:boolean = false
+  isBuild: boolean = false
 
   //onclick toggling both
-   onclickBuildCheck()
-   {
-     this.isBuild = !this.isBuild
-   }
+  onclickBuildCheck() {
+    this.isBuild = !this.isBuild
+  }
 
-   //hiding egg_build
-  isTaskFlow:boolean = false
-
-  //onclick toggling both
-   onclickTaskFlowCheck()
-   {
-     this.isTaskFlow = !this.isTaskFlow
-   }
-
-    //hiding egg_build
-  isTaskDQ:boolean = false
+  //hiding egg_build
+  isTaskFlow: boolean = false
 
   //onclick toggling both
-   onclickTaskDQCheck()
-   {
-     this.isTaskDQ = !this.isTaskDQ
-   }
+  onclickTaskFlowCheck() {
+    this.isTaskFlow = !this.isTaskFlow
+  }
+
+  //hiding egg_build
+  isTaskDQ: boolean = false
+
+  //onclick toggling both
+  onclickTaskDQCheck() {
+    this.isTaskDQ = !this.isTaskDQ
+  }
   data = {
     id: 1,
     env: '',
     repo: '',
     branch: '',
-    localPath:'',
+    localPath: '',
     eggFilePath: '',
     dbfsPath: '',
     inputJson: '',
     flowJson: '',
-    inputDqJson:''
+    inputDqJson: '',
+    isRepo: false,
+    isTaskFlow: false,
+    isTaskDQ: false,
+    isBuild: false
 
   };
+
+  responseObj = {
+    task_env_region: '',
+    task_name: '',
+    databricks_runid: 0,
+    databricks_link: '',
+    databricks_state: '',
+    databricks_log: ''
+  }
+
+  responseObjArr: any = [
+    {
+      "task_env_region": "EU-UAT",
+      "task_name": "CS_AMS_INTERACTION_MATRIX",
+      "databricks_runid": 68217538,
+      "databricks_link": "https://adb-5892249866138306.6.azuredatabricks.net/?o=5892249866138306#job/30363921008757/run/68217538",
+      "databricks_state": "SUCCESS",
+      "databricks_log": "test log"
+    }
+  ];
+
+
+  columnDefs = [
+    {
+      "headerName": "Id",
+      "headerTooltip": "Id",
+      "field": "databricks_runid",
+      "width": 100,
+      "resizable": true,
+      "tooltipField": "id"
+    },
+    {
+      "headerName": "ENV",
+      "headerTooltip": "task_env_region",
+      "field": "task_env_region",
+      "width": 100,
+      "resizable": true,
+      "tooltipField": "task_env_region"
+    },
+    {
+      "headerName": "Status",
+      "headerTooltip": "databricks_state",
+      "field": "databricks_state",
+      "width": 100,
+      "resizable": true,
+      "tooltipField": "databricks_state"
+    },
+    {
+      "headerName": "Batabricks link",
+      "headerTooltip": "databricks_link",
+      "field": "databricks_link",
+      "cellRenderer":(params: ICellRendererParams) => {
+        params.data  
+        return `<a href="${params.data.databricks_link}">${params.value}</a>`},
+      "width": 350,
+      "resizable": true,
+      "tooltipField": "databricks_link"
+    },
+    {
+      "headerName": "Task Name",
+      "headerTooltip": "task_name",
+      "field": "task_name",
+      "width": 250,
+      "resizable": true,
+      "tooltipField": "task_name"
+    },
+    {
+      "headerName": "Databricks Log",
+      "headerTooltip": "databricks_log",
+      "field": "databricks_log",
+      "width": 300,
+      "resizable": true,
+      "tooltipField": "databricks_log"
+    },
+
+  ]
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     // this.createForm();
@@ -75,12 +152,17 @@ export class AppComponent {
       env: '',
       repo: '',
       branch: '',
-      localPath:'',
+      localPath: '',
       eggFilePath: '',
       dbfsPath: '',
       inputJson: '',
       flowJson: '',
-      inputDqJson:''
+      inputDqJson: '',
+      isRepo: false,
+      isTaskFlow: false,
+      isTaskDQ: false,
+      isBuild: false
+
     });
   }
 
@@ -99,16 +181,27 @@ export class AppComponent {
       dbfsPath: this.data.dbfsPath,
       inputJson: this.data.inputJson,
       flowJson: this.data.flowJson,
-      inputDqJson:this.data.inputDqJson
+      inputDqJson: this.data.inputDqJson,
+      isRepo: this.data.isRepo,
+      isTaskFlow: this.data.isTaskFlow,
+      isTaskDQ: this.data.isTaskDQ,
+      isBuild: this.data.isBuild
+
     });
   }
-
 
   onSubmit(myForm: FormGroup) {
     console.log('Your form data : ', myForm.value);
     const body = myForm.value;
     this.http.post<any>('http://localhost:7777/post_data/', body).subscribe(data => {
       this.response = data;
+    });
+  }
+
+  onClickResult(){
+    this.isForum=false;
+    this.http.get<any>('http://localhost:7777/result_data').subscribe(data => {
+      this.responseObjArr = data;
     });
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; 
+import { AgGridModule } from 'ag-grid-angular-legacy';
+import { NgbNavModule  } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -15,7 +17,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AgGridModule,
+    NgbNavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
